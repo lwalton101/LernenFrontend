5156925e59de1ee819edd50fde746ffb63195dfc
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-20 16:03:46 +0100

    P1: added env variables

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -4,7 +4,7 @@ import axios, { AxiosInstance } from 'axios';
 
 // Define an Axios instance with default configurations
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: process.env.BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
