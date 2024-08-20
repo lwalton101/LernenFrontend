816d9462f31ef3a74fc4b9b3589c093e9bc47c6e
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-20 20:07:28 +0100

    P2: added navigation to navbar

--- NavbarItem.tsx
+++ NavbarItem.tsx
@@ -1,11 +1,14 @@
+import {useNavigate} from "react-router-dom";
+
 interface NavbarItemProps {
     title: string,
     path?: string
 }
 
 export function NavbarItem(props: NavbarItemProps) {
+    const navigator = useNavigate();
     return (
-        <div className={"h-full w-10 justify-center items-center p-5 flex hover:bg-primary_dark"}>
+        <div className={"h-full w-10 justify-center items-center p-5 flex hover:bg-primary_dark"} onClick={() => navigator(props.path ? props.path : "error")}>
             <p className={"text-center text-black"}>
                 {props.title}
             </p>
