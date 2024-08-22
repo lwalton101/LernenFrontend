0c51c59b75ed9f48481ff8123cfb8d0923eb37eb
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-22 15:01:20 +0100

    P2:added defualt pfp to navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,10 +1,13 @@
 import {NavbarItem} from "../NavbarItem.tsx";
+import defaultPFP from "../src/assets/default_pfp.png"
+import {useUser} from "./context/UserContext.tsx";
 
 export function Navbar() {
+    const user = useUser();
     return (
         <>
             <div className="flex justify-between items-center bg-primary">
-                <div className={"flex flex-wrap flex-1 h-10 w-1/3 items-center"}>
+                <div className={"flex flex-wrap flex-1 h-12 w-1/3 items-center"}>
                     <NavbarItem title={"Home"} path={"/root"}></NavbarItem>
                     <NavbarItem title={"Login"} path={"/login"}></NavbarItem>
                     <NavbarItem title={"Create"} path={"/create"}></NavbarItem>
@@ -12,6 +15,10 @@ export function Navbar() {
                 </div>
                 <div className="flex-1 text-center">Search(todo)</div>
                 <div className={"flex flex-wrap flex-row-reverse flex-1 h-10 w-1/3"}>
+                    <img className={"h-full p-1"} src={defaultPFP} alt={"The default profile picture"}></img>
+                    <div className={"justify-center items-center flex mr-2"}>
+                        <p className={"text-center text-black"}>{user ? user.username : ""}</p>
+                    </div>
                     <NavbarItem title={"Profile"} path={"/profile"}></NavbarItem>
                 </div>
             </div>
