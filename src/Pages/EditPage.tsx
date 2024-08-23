d7f844958d51330ab5e9799f681281f234431ccd
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-23 13:52:16 +0100

    P3: added a empty SubquestionEditor.tsx

--- src/Pages/EditPage.tsx
+++ src/Pages/EditPage.tsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import {useSearchParams} from "react-router-dom";
 import {QuestionProvider} from "../context/QuestionContext.tsx";
 import {MainInfoEditor} from "./MainInfoEditor.tsx";
+import {SubquestionEditor} from "./SubquestionEditor.tsx";
 
 export function EditPage() {
     const [searchParams, _] = useSearchParams();
@@ -17,6 +18,7 @@ export function EditPage() {
                     <Navbar></Navbar>
                     <div className={"h-full flex"}>
                         <MainInfoEditor></MainInfoEditor>
+                        <SubquestionEditor></SubquestionEditor>
                     </div>
 
                 </div>
---
+++ src/Pages/SubquestionEditor.tsx
@@ -0,0 +1,11 @@
+export function SubquestionEditor() {
+    return (
+        <>
+            <div>
+                <h1>
+                    Subquestion Editor
+                </h1>
+            </div>
+        </>
+    );
+}
