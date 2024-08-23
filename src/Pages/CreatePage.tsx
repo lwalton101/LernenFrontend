7c779b5e7b4580e8c793fcfd43b5b0a306455249
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-23 12:27:48 +0100

    P3: CreatePage layout and post request done

--- src/Pages/CreatePage.tsx
+++ src/Pages/CreatePage.tsx
@@ -1,9 +1,40 @@
 import {Navbar} from "../Navbar.tsx";
+import {useState} from "react";
+import axiosInstance from "../axiosInstance.ts";
+import {useUser} from "../context/UserContext.tsx";
 
 export function CreatePage() {
+    const [title, setTitle] = useState("");
+    const user = useUser();
+    async function onCreatePressed() {
+        alert(user)
+        await axiosInstance.post("/question/create", {
+            title: title,
+            user_id: user?.user_id,
+            created_at: Date.now(),
+            published: false,
+            tags: ["first-question"],
+            subquestions: []
+        });
+
+
+    }
+
     return (
         <>
-            <Navbar></Navbar>
+            <div className={"h-screen"}>
+                <Navbar></Navbar>
+                <div className={"h-full flex items-center justify-center"}>
+                    <div className={"bg-primary rounded p-3 text-center"}>
+                        <h1 className={"text-xl mb-2"}>Create Question</h1>
+                        <input type={"text"} placeholder={"Title"} className={"p-1"} onChange={(e) => setTitle(e.target.value)}/>
+                        <button
+                            className={"self-end mx-auto p-3 bg-primary_dark rounded-full mt-2 w-1/3 text-white"} onClick={onCreatePressed}>
+                            Create
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
