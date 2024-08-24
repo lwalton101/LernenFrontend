751711efc8f9b2640dbf46f12d1135c1b3f6a554
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-24 18:19:26 +0100

    P4: add random questions to browse page

--- src/Pages/RootPage.tsx
+++ src/Pages/RootPage.tsx
@@ -10,17 +10,20 @@ export function RootPage(){
     const {user} = useUser();
     const [questions, setQuestions] = useState([])
     useEffect(() => {
-        axiosInstance.get("/question/browse/1").then((r: AxiosResponse) => setQuestions(r.data)).catch((e) => console.log(e));
+        axiosInstance.get("/question/browse/10").then((r: AxiosResponse) => setQuestions(r.data)).catch((e) => console.log(e));
     }, []);
     return(
         <>
             <div className={"flex flex-col h-screen"}>
                 <Navbar></Navbar>
                 <div className={"flex flex-col h-full items-center"}>
-                    <h1>
-                        Welcome back, {user ? user.username : ""}
+                    <h1 className={"font-bold text-5xl mt-3"}>
+                        Welcome back, {user ? user.username : ""}!
                     </h1>
-                    <div className={"flex gap-15 flex-wrap items-center"}>
+                    <p className={"text-2xl"}>
+                        Why not try one of these questions?
+                    </p>
+                    <div className={"flex gap-15 flex-wrap justify-center"}>
                         {questions.map((question: Question) => (
                             <div key={question.question_id} className={"f"}>
                                 <QuestionBox question={question}/>
