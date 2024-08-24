afb9beee8cbbebcfc2f1e7ae2aa6f5daaad16aab
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-24 17:44:23 +0100

    P4: made question box look nice

--- src/Pages/QuestionBox.tsx
+++ src/Pages/QuestionBox.tsx
@@ -1,13 +1,45 @@
-import {Question} from "../Model/Question.ts";
+import {getDate, Question} from "../Model/Question.ts";
+import {useEffect, useState} from "react";
+import axiosInstance from "../axiosInstance.ts";
+import {AxiosResponse} from "axios";
+import {User} from "../Model/User.ts";
+import defaultPFP from "../assets/default_pfp.png"
 
 interface QuestionBoxProps {
     question: Question;
 }
 
 export function QuestionBox({question}: QuestionBoxProps) {
+    const [uploader, setUploader] = useState<User | null>(null)
+    useEffect(() => {
+        axiosInstance.get(`/user/${question.user_id}`).then((r: AxiosResponse<User>) => setUploader(r.data)).catch((e) => console.log(e));
+    }, []);
+
     return (
-        <div>
-            <p>{question.title}</p>
+        <div className={"bg-primary flex flex-col items-center m-4 rounded-xl w-[300px] h-[400px]"}>
+            <div className={" flex flex-col p-3 items-center flex-1 w-full"}>
+                <p className={"text-3xl text-center"}>{question.title}</p>
+                <hr className="border-t-4 border-secondary w-11/12 my-2"/>
+                <div className={"flex flex-row-reverse items-center"}>
+                    {uploader?.profile_pic ? (<></>) : (
+                        <img src={defaultPFP} alt={"The default profile picture"} className={"h-10"}></img>)}
+                    <p className={"m-2"}>Author: {uploader?.username}</p>
+                </div>
+                <p className={"m-1"}>Subquestions: {question.subquestions.length}</p>
+                <p className={"m-1"}>Upload Date: {getDate(question.created_at)}</p>
+                <div className={"flex flex-col items-center h-full w-full"}>
+                    <p className={"m-1 text-xl"}>Tags</p>
+                    <div className={"flex flex-wrap justify-center"}>
+                        {question.tags.map((tag) => (
+                            <p key={tag} className={"bg-gray-200 m-1 p-2 rounded-sm text-sm truncate flex-shrink min-w-0"}>
+                                {tag}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <button className={"bg-primary_dark rounded-full m-1 text-white p-2 w-5/6 "}>Play</button>
         </div>
+
     );
 }
