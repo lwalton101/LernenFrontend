54fe0d989f2d7132b0eb8c2c775f147ef6045469
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-24 16:00:34 +0100

    P4: Add question boxes to RootPage

--- src/Pages/RootPage.tsx
+++ src/Pages/RootPage.tsx
@@ -1,8 +1,17 @@
 import {Navbar} from "../Navbar.tsx";
 import {useUser} from "../context/UserContext.tsx";
+import {QuestionBox} from "./QuestionBox.tsx";
+import {useEffect, useState} from "react";
+import axiosInstance from "../axiosInstance.ts";
+import {Question} from "../Model/Question.ts";
+import {AxiosResponse} from "axios";
 
 export function RootPage(){
     const {user} = useUser();
+    const [questions, setQuestions] = useState([])
+    useEffect(() => {
+        axiosInstance.get<Question[]>("/question/browse/1").then((r: AxiosResponse<Question[]>) => setQuestions(r.data)).catch((e) => console.log(e));
+    }, []);
     return(
         <>
             <div className={"flex flex-col h-screen"}>
@@ -11,6 +20,11 @@ export function RootPage(){
                     <h1>
                         Welcome back, {user ? user.username : ""}
                     </h1>
+                    {questions.map((question: Question) => (
+                        <div key={question.question_id}>
+                            <QuestionBox question={question}/>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
