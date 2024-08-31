3db2fc3d14106dc26ab75ec54f5e5a4f483da129
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-31 15:09:05 +0100

    P5: added Rating to QuestionBox.tsx

--- src/Pages/QuestionBox.tsx
+++ src/Pages/QuestionBox.tsx
@@ -5,6 +5,7 @@ import {AxiosResponse} from "axios";
 import {User} from "../Model/User.ts";
 import defaultPFP from "../assets/default_pfp.png"
 import {useNavigate} from "react-router-dom";
+import {Rating} from "../Model/Rating.ts";
 
 interface QuestionBoxProps {
     question: Question;
@@ -12,9 +13,11 @@ interface QuestionBoxProps {
 
 export function QuestionBox({question}: QuestionBoxProps) {
     const [uploader, setUploader] = useState<User | null>(null)
+    const [rating, setRating] = useState<Rating | null>(null)
     const navigator = useNavigate();
     useEffect(() => {
         axiosInstance.get(`/user/${question.user_id}`).then((r: AxiosResponse<User>) => setUploader(r.data)).catch((e) => console.log(e));
+        axiosInstance.get(`/rating/${question.question_id}`).then((r: AxiosResponse<Rating>) => setRating(r.data)).catch((e) => console.log(e));
     }, []);
 
     return (
@@ -29,6 +32,10 @@ export function QuestionBox({question}: QuestionBoxProps) {
                 </div>
                 <p className={"m-1"}>Subquestions: {question.subquestions.length}</p>
                 <p className={"m-1"}>Upload Date: {getDate(question.created_at)}</p>
+                <div className={"flex w-full gap-3 justify-center"}>
+                    <p>Readability: {rating?.readability}/5</p>
+                    <p>Difficulty: {rating?.difficulty}/5</p>
+                </div>
                 <div className={"flex flex-col items-center h-full w-full"}>
                     <p className={"m-1 text-xl"}>Tags</p>
                     <div className={"flex flex-wrap justify-center"}>
@@ -40,7 +47,7 @@ export function QuestionBox({question}: QuestionBoxProps) {
                     </div>
                 </div>
             </div>
-            <button className={"bg-primary_dark rounded-full m-1 text-white p-2 w-5/6 "} onClick={() => navigator(`/play?id=${question.question_id}`)}>Play</button>
+            <button className={"bg-primary_dark rounded-full m-1 text-white p-2 mb-3 w-5/6 "} onClick={() => navigator(`/play?id=${question.question_id}`)}>Play</button>
         </div>
 
     );
