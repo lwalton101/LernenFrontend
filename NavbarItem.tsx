b6fb8d913291865b318dfe7026dbd8df32c9d780
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-09-02 18:06:27 +0100

    P6: upgrade navbar item width

--- NavbarItem.tsx
+++ NavbarItem.tsx
@@ -7,7 +7,7 @@ interface NavbarItemProps {
 
 export function NavbarItem(props: NavbarItemProps) {
     const navigator = useNavigate();
-    const className = "h-full w-10 justify-center items-center p-5 flex hover:bg-primary_dark" +(("/" + window.document.URL.split("/")[3]).startsWith(props.path) ? " bg-primary_dark" : " testt");
+    const className = "h-full w-30 justify-center items-center p-5 flex hover:bg-primary_dark" +(("/" + window.document.URL.split("/")[3]).startsWith(props.path) ? " bg-primary_dark" : " testt");
     return (
         <div className={className} onClick={() => navigator(props.path ? props.path : "error")}>
             <p className={"text-center text-black"}>
